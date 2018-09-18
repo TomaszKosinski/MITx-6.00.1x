7391ce1cea1e4f1612de0d65e32abf1916f57003
Tomasz Kosinski <tomasz.kosinski@gmail.com>
2018-09-19 00:22:40 +0200

    Week2/ProblemSet2/Problem3 Minimal monthly payment to pay off credit card in a year - bisection method

--- ProblemSet2/Problem3/PayingDebtOffInYearBisection.py
+++ ProblemSet2/Problem3/PayingDebtOffInYearBisection.py
@@ -20,20 +20,24 @@ def minMonthlyPayment():
     lowerBoundPayment = round(balance / 12.0, 2)
     # we have to pay at most this (probably less)
     upperBoundPayment = round(balance * ((1 + (annualInterestRate/12))**12) / 12.0, 2) 
-    
+    numberOfGuesses = 0
     while (True):
+        numberOfGuesses += 1
         currentGuess = ( upperBoundPayment + lowerBoundPayment ) / 2.0
         retBalance = calculateBalanceAfterYear(currentGuess, balance, annualInterestRate)
         if(upperBoundPayment - lowerBoundPayment <= 0.01):
             print("Lowest Payment: " + str(round(upperBoundPayment, 2)))
             break
-        elif retBalance == 0:
-            print("Lowest Payment: " + str(retBalance))
+        # it is ok to not be very precise
+        elif retBalance > -0.1 and retBalance < 0.1:
+            print("Lowest Payment: " + str(round(currentGuess,2)))
             break
-        elif retBalance < 0:
-            lowerBoundPayment = currentGuess
+        elif retBalance > 0:
+            lowerBoundPayment += (upperBoundPayment - lowerBoundPayment) / 2
         else :
-            upperBoundPayment = currentGuess
-
+            upperBoundPayment -= (upperBoundPayment - lowerBoundPayment) / 2
+        
+    #print("Number of guesses: " + str(numberOfGuesses))
 
 minMonthlyPayment()    
+
